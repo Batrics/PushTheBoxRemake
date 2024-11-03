bc795a630e7ff9183ef60338d4cd4d1be16911d4
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-11-03 09:13:55 +0700

    .

--- Assets/Bagas/Script/ButtonScript.cs
+++ Assets/Bagas/Script/ButtonScript.cs
@@ -8,25 +8,60 @@ public class ButtonScript : MonoBehaviour
     public string TargetElement;
     public ButtonDoorScript buttonDoorScript;
 
-    private void Start() {
+    // New field for default material color
+    public Color defaultColor = Color.white; // Default color
+    private Renderer buttonRenderer; // Renderer to change material color
+
+    private void Start()
+    {
         buttonDoorScript = transform.parent.GetComponent<ButtonDoorScript>();
+        buttonRenderer = GetComponent<Renderer>(); // Get the Renderer component
+        if (buttonRenderer != null)
+        {
+            // Set the default color from the Inspector
+            buttonRenderer.material.color = defaultColor;
+        }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Box")) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
             BoxElement boxElement = other.GetComponent<BoxElement>();
-            if(TargetElement == boxElement.element) {
+            if (TargetElement == boxElement.element)
+            {
+                AudioManager.instance.PlaySFX(2);
                 isClick = true;
                 buttonDoorScript.clickedInt++;
+
+                // Change material color to white
+                if (buttonRenderer != null)
+                {
+                    buttonRenderer.material.color = Color.white;
+                }
+            }
+            else
+            {
+                AudioManager.instance.PlaySFX(3);
             }
         }
     }
-    private void OnTriggerExit(Collider other) {
-        if(other.gameObject.CompareTag("Box")) {
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Box"))
+        {
             BoxElement boxElement = other.GetComponent<BoxElement>();
-            if(TargetElement == boxElement.element) {
+            if (TargetElement == boxElement.element)
+            {
                 isClick = false;
                 buttonDoorScript.clickedInt--;
+
+                // Revert to default material color
+                if (buttonRenderer != null)
+                {
+                    buttonRenderer.material.color = defaultColor;
+                }
             }
         }
     }
